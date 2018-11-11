755c16185f00566916f5f4afa98cb35ca2e77cf1
OhYeonJeong1220 <wroldpay@gmail.com>
2018-11-12 01:28:28 +0900

    make case 5(reg(eax) to mem) and case 6(immediate to reg)

--- trans.c
+++ trans.c
@@ -1,21 +1,33 @@
 #include "myas.h"
 
 int instr_trans(char *op, char *args, char* mcode)
-{
-	// check syntax 
-	if(!is_valid(op, args)){
-		printf("Error: %s %s is not valid\n", op, args);
-		return 0;
+{//op:mov , args: next string, mcode: Opcode
+    char seps[]=",";//delimit
+    char *token;
+
+    // check syntax
+    //is_vaild() is in check.c 
+    if(!is_valid(op, args)){
+    	printf("Error: %s %s is not valid\n", op, args);
+    	return 0;
+    }
+
+    strcpy(mcode, "AB CD EF");
+    
+    token = strtok(args, seps);
+    while(token != NULL){
+	if(strcmp(token,"%eax")==0){
+	//reg(eax) to mem
+	    strcpy(mcode,"a3");
+	    break;
 	}
-
-
-	strcpy(mcode, "AB CD EF");
-
-	/********************************/
-    /*  							*/
-	/*    generate machine code     */
-    /*  							*/
-	/********************************/
-	
-	return 1;	
+	else{
+	   //immediate to reg
+	    if(strchr(token,'$')!=NULL){
+		strcpy(mcode,"b8");
+		break;
+	    }	
+	}
+    }
+    return 1;	
 }
