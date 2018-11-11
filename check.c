bcf489c4a2c9b65a7bc99a6c2a222f6d3ce4bfbe
OhYeonJeong1220 <wroldpay@gmail.com>
2018-11-12 01:30:03 +0900

    make error sentance about instruction is not mov

--- check.c
+++ check.c
@@ -2,14 +2,12 @@
 
 int is_valid(char *op, char *args)
 {
+	//case1 : first String is not mov
+	if(strcmp(op,"mov")!=0){
+		printf("Instruction is not mov!!\n");
+		return 0;
+	}
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
-
-	/********************************/
-    /*  							*/
-	/*         syntax check         */
-    /*  							*/
-	/********************************/
-	
 	return 1;
 }
