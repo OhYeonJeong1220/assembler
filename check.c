1dd31c576ac2e6733a754268ffb7181d737b2840
OhYeonJeong1220 <wroldpay@gmail.com>
2018-11-13 01:31:37 +0900

    change mem to mem error and complete

--- check.c
+++ check.c
@@ -2,44 +2,34 @@
 
 int is_valid(char *op, char *args)
 {
-   // char seps[]=",";//delimit
-    char *token,*token1,*token2;
+    char seps[]=",";//delimit
+    char *ctoken1,*ctoken2;
+    char *n;
+    n=(char *)malloc(sizeof(char)*50);
+    ctoken1=(char *)malloc(sizeof(char)*50);
+    ctoken2=(char *)malloc(sizeof(char)*50);
+    strcpy(n,args);
+
     //make token
-
-    token=strtok(args,",");
-    token1=token;
-    token=strtok(NULL,",");
-    token2=token;
-
+    strcpy(ctoken1,strtok(n,seps));
+    strcpy(ctoken2,strtok(NULL,seps));
+   
     //case1 : first String is not mov
     if(strcmp(op,"mov")!=0){
 	    printf("Error: Instruction is not mov!!\n");
 	    return 0;
     }
-    //No register
-    else if((strchr(token1,'%')==NULL)&&(strchr(token2,'%')==NULL)){
-	//mem to mem
-	if((strchr(token1,'(')!=NULL)&&(strchr(token2,'(')!=NULL)){
-	printf("Error: mem to mem\n");
-	return 0;
-	}
-	else if((strchr(token1,'(')!=NULL)&&(strchr(token2,'x')!=NULL)){
-	printf("Error: mem to mem\n");
-	return 0;
-	}
-	else if((strchr(token1,'x')!=NULL)&&(strchr(token2,'(')!=NULL)){
-	printf("Error: mem to mem\n");
-	return 0;
-	}
-	else if((strchr(token1,'x')!=NULL)&&(strchr(token2,'x')!=NULL)){
-	printf("Error: mem to mem\n");
-	return 0;
-	}
-	//somethihg to immediate
-	if(strchr(token2,'$')!=NULL){
-	printf("Error: Destination is immediate\n");
-	return 0;
-	}
+    //destination is immediate
+    if(strchr(ctoken2,'$')!=NULL){
+    printf("Error: Destination is immediate\n");
+    return 0;
+    }
+    else{
+    //mem to mem
+    if(ctoken1[0]!='%'&&ctoken2[0]!='%'){
+    printf("Error: mem to mem\n");
+    return 0;
+    }
     }
     //printf("if valid, return 1\n");
     //printf("otherwise, return 0\n");
